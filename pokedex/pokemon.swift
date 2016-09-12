39fb7fc5229215801c8f92df72946e84d406dfb6
Maia <raafamaia@gmail.com>
2016-09-11 21:30:57 -0300

    added pokemon model

---
+++ pokedex/pokemon.swift
@@ -0,0 +1,36 @@
+//
+//  pokemon.swift
+//  pokedex
+//
+//  Created by R. Maia on 9/11/16.
+//  Copyright © 2016 RM. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    //#MARK: Private Atributes
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    //#MARK: Getters/Setters
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    var pokedexId: Int {
+        get {
+            return _pokedexId
+        }
+    }
+    
+    //#MARK: Constructors
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+
+}
